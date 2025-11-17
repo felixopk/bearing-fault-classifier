fe401b9aefa4a61dca2bdebdb4588cee1613fe8f
Felix oppong <felixoppong2000@yahoo.com>
2025-11-17 14:09:10 +0100

    updated

--- aws-infrastructure/setup-ecs.sh
+++ aws-infrastructure/setup-ecs.sh
@@ -9,7 +9,7 @@ CLUSTER_NAME="bearing-classifier-cluster"
 SERVICE_NAME="bearing-classifier-service"
 TASK_FAMILY="bearing-classifier-task"
 ECR_REPO="bearing-classifier"
-CONTAINER_NAME="bearing-api"
+CONTAINER_NAME="bearing-classifier-container"
 CONTAINER_PORT=8000
 
 echo "🚀 Setting up AWS ECS Infrastructure"
